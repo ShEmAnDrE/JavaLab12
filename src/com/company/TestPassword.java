d89c071ebdb41456438e20c722a270d7da19045c
ShEmAnDrE <tyfly58@mail.ru>
2022-10-20 19:25:42 +0300

    Initial commit

---
+++ src/com/company/TestPassword.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class TestPassword {
+    public static boolean checkPass(String s){
+        return Pattern.compile("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z_]{8,}").matcher(s).find();
+    }
+
+    public static void main(String[] args) {
+        boolean f = true;
+        while (f) {
+            System.out.println("\nВыберите одну из двух команд:");
+            System.out.println("1 - Проверка пароля");
+            System.out.println("Любая другая клавиша - выход из программы");
+            Scanner sc = new Scanner(System.in);
+            char choice = sc.nextLine().charAt(0);
+            if (choice == '1') {
+                System.out.println("Введите пароль для проверки");
+                String password = sc.nextLine();
+                if (checkPass(password)) {
+                    System.out.println("Пароль \"" + password + "\" надежен");
+                } else {
+                    System.out.println("Пароль \"" + password + "\" ненадежен");
+                }
+            } else {
+                System.out.println("До свидания");
+                f = false;
+            }
+        }
+    }
+}
